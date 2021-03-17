70165917ab6e09fd502f45c06c5d1a8537d89db9
danielvonth <danielvonth@protonmail.com>
2021-03-17 12:33:59 +0100

    Toast Added

--- app/src/main/java/de/task/screens/Surprise.kt
+++ app/src/main/java/de/task/screens/Surprise.kt
@@ -1,9 +1,11 @@
 package de.task.screens
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
@@ -17,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,24 +35,34 @@ import be.sigmadelta.calpose.widgets.DefaultHeader
 import kotlinx.coroutines.flow.MutableStateFlow
 import java.time.DayOfWeek
 
+
 import java.time.YearMonth
 
 
 
 @Composable
-fun ButtonExample() {
-    Button(onClick = {
-
-                     }, colors = ButtonDefaults.textButtonColors(
-        backgroundColor = Color.Red
-    )) {
-        Text("Button")
+fun SurpriseButton() {
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val context = LocalContext.current
+        Button(
+            onClick = {
+                Toast.makeText(context,"Lass dich überraschen!",Toast.LENGTH_LONG).show()
+            }, colors = ButtonDefaults.textButtonColors(
+                backgroundColor = Color.Red
+            )
+        ) {
+            Text("Surprise Me!")
+        }
     }
 }
 
 @Composable
 fun surprise(){
-    ButtonExample()
+    SurpriseButton()
 }
 
 
