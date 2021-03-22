9e64850c675117a8d2bb5880b389091a22befecc
Lena <lena_schollmayer@web.de>
2021-03-22 20:04:23 +0100

    mockup daily screen in progress

---
+++ app/src/main/java/de/task/screens/Daily.kt
@@ -0,0 +1,83 @@
+package de.task.screens
+
+
+import android.graphics.drawable.BitmapDrawable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import de.task.R
+
+
+@Composable
+fun DummyCalendar() {
+
+
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+
+        Row(modifier = Modifier
+            .align(Alignment.CenterHorizontally)
+            .padding(top = 20.dp)
+        ) {
+            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.backicon), contentDescription = "backIcon",
+                modifier = Modifier
+                    .size(20.dp)
+            )
+            Text(text = "heutiges Datum als String?", modifier = Modifier.padding(start = 10.dp, end = 10.dp) )
+            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.nexticon), contentDescription = "nextIcon",
+                modifier = Modifier
+                    .size(30.dp)
+            )
+        }
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .padding(top = 20.dp)
+                .clickable {
+
+                },
+            shape = RoundedCornerShape(15.dp),
+            backgroundColor = Color.LightGray
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+
+                    }
+            ) {
+                Image(
+                    bitmap = ImageBitmap.imageResource(id = R.drawable.task),
+                    contentDescription = "TaskImage",
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .size(100.dp)
+                        .padding(start = 5.dp)
+                )
+                Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    Text(text = "task", fontSize = 24.sp)
+                    Text(text = "Dauer:   1h30")
+                    Text(text = "Kategorie:  SelfCare")
+                }
+
+            }
+        }
+    }
+}
+
